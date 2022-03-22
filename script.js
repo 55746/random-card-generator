3f51ffd74427f9b093e4cc317986c0429e4f0ade
55746 <Lawhead.gavin@live.jobcorps.org>
2022-03-22 00:45:30 +0000

    second commit

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
-// function randomnum() {
-//   let number = document.querySelector("#number");
-//   var randomNumber = Math.floor(Math.random() * 10).number;
-//   return element.write(randomNumber());
-// }
+let cards = ["A", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, "J", "K", "Q"];
+let suitblack = ["♠", "♣"];
+let suitred = ["♦", "♥"];
+let fullsuit = ["♠", "♣", "♦", "♥"];
 
-// let element = getquerySelector("#card")
-function ranNumber() {
-  // random number generator
-  let number = Math.floor(Math.random() * 10);
-  //how to add it to the div. no hashtag
-  document.getElementById("#number").innerText = number;
-  return;
+let suitorigin = suitblack[Math.floor(Math.random() * suitblack.length)];
+let suitorigin2 = suitred[Math.floor(Math.random() * suitred.length)];
+
+for (i = 0; i <= fullsuit.length; i++) {
+  if (fullsuit.indexOf == suitred[("♦", "♥")]) {
+    document.querySelector("#top-suitred").innerText = suitorigin2;
+  } else fullsuit.indexOf == suitblack[("♠", "♣")];
+  document.querySelector("#bottom-suitblack").innerText = suitorigin;
 }
 
-//when using onload method to this
-ranNumber();
+// ARRAYS
+// let cardsorigin = cards[Math.floor(Math.random() * cards.length)];
+
+// MAIN MATH SYNTAX
+
+// document.querySelector("#top-suitred").innerText = suitorigin2;
+// document.querySelector("#top-suitblack").innerText = suitorigin;
+// document.querySelector("#bottom-suitblack").innerText = suitorigin;
+// document.querySelector("#number").innerText = cardsorigin;
+
+// // COMBINING MATH SYNTAX WITH CORRECT ID
+
+// function generate() {
+// if (suit.indexOf == suit[("♦", "♥")]) {
+//     document.querySelector("#top-suitred").innerText = suitorigin2;
+// }else if(suit.indexOf == suit[("♠", "♣")]){
+//     document.querySelector("#bottom-suit").innerText = suitorigin;
+// }
+
+//   document.querySelector("#top-suit").innerText = suitorigin;
+//   document.querySelector("#bottom-suit").innerText = suitorigin;
+//   document.querySelector("#number").innerText = cardsorigin;
+// }
