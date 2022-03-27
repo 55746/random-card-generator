b798048612484a7b798d88b7bf3d90ad59c09da9
55746 <Lawhead.gavin@live.jobcorps.org>
2022-03-27 18:53:15 +0000

    finished

--- script.js
+++ script.js
@@ -1,38 +1,11 @@
 let cards = ["A", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, "J", "K", "Q"];
-let suitblack = ["♠", "♣"];
-let suitred = ["♦", "♥"];
 let fullsuit = ["♠", "♣", "♦", "♥"];
-
-let suitorigin = suitblack[Math.floor(Math.random() * suitblack.length)];
-let suitorigin2 = suitred[Math.floor(Math.random() * suitred.length)];
-
-for (i = 0; i <= fullsuit.length; i++) {
-  if (fullsuit.indexOf == suitred[("♦", "♥")]) {
-    document.querySelector("#top-suitred").innerText = suitorigin2;
-  } else fullsuit.indexOf == suitblack[("♠", "♣")];
-  document.querySelector("#bottom-suitblack").innerText = suitorigin;
+let suitorigin = fullsuit[Math.floor(Math.random() * fullsuit.length)];
+document.querySelector("#suit").innerHTML = suitorigin;
+document.querySelector("#suit2").innerHTML = suitorigin;
+if (suitorigin === fullsuit[2] || suitorigin === fullsuit[3]) {
+  document.querySelector("#suit").style.color = "red";
+  document.querySelector("#suit2").style.color = "red";
 }
-
-// ARRAYS
-// let cardsorigin = cards[Math.floor(Math.random() * cards.length)];
-
-// MAIN MATH SYNTAX
-
-// document.querySelector("#top-suitred").innerText = suitorigin2;
-// document.querySelector("#top-suitblack").innerText = suitorigin;
-// document.querySelector("#bottom-suitblack").innerText = suitorigin;
-// document.querySelector("#number").innerText = cardsorigin;
-
-// // COMBINING MATH SYNTAX WITH CORRECT ID
-
-// function generate() {
-// if (suit.indexOf == suit[("♦", "♥")]) {
-//     document.querySelector("#top-suitred").innerText = suitorigin2;
-// }else if(suit.indexOf == suit[("♠", "♣")]){
-//     document.querySelector("#bottom-suit").innerText = suitorigin;
-// }
-
-//   document.querySelector("#top-suit").innerText = suitorigin;
-//   document.querySelector("#bottom-suit").innerText = suitorigin;
-//   document.querySelector("#number").innerText = cardsorigin;
-// }
+let cardorigin = cards[Math.floor(Math.random() * cards.length)];
+document.querySelector("#number").innerText = cardorigin;
